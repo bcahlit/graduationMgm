c12c8cd212a82ed5b9e7a7504fbdb3bfd9129bda
liang_zx <lzxworkable@163.com>
2020-08-12 16:08:48 +0800

    lzx分支初始化

---
+++ shells/run_hfut_agent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+export PYTHONPATH=$PYTHONPATH:$DIR/..
+HFO_mgm/bin/HFO --fullstate --no-logging --headless --defense-agents=0 --offense-npcs=0 --defense-npcs=1 --offense-agents=1 --offense-team=$1 --defense-team=$2 --trials $3 &
+# Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
+sleep 5
+echo "python agent"
+python ./agents/agent.py $2&
+sleep 1
+# python ./agents/agent.py $2&
+# sleep 1
+# python ./agents/agent.py $2&
+# sleep 1
+# python ./agents/agent.py $2&
+# sleep 1
+# The magic line
+#   $$ holds the PID for this script
+#   Negation means kill by process group id instead of PID
+trap "kill -TERM -$$" SIGINT
+wait
---
+++ shells/run_hfut_npc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $DIR
+export PYTHONPATH=$PYTHONPATH:$DIR/..
+HFO_mgm/bin/HFO --fullstate --no-logging --headless --defense-agents=0 --offense-npcs=0 --defense-npcs=1 --offense-agents=1 --offense-team=helios --defense-team=hfut --trials 100 &
+
+# Sleep is needed to make sure doesn't get connected too soon, as unum 1 (goalie)
+sleep 5
+echo "python agent"
+python ./agents/agent.py helios&
+sleep 1
+
+trap "kill -TERM -$$" SIGINT
+wait
